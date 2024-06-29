07fc7c0a508d1b3acaf6b310f3063dd1b5bd86d6
Sanjida Akter <sanjidakter2304@gmail.com>
2024-06-29 16:01:02 +0600

    post api working successfully

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -11,25 +11,45 @@ import {
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useAppDispatch} from "@/redux/hook";
+// import { useAppDispatch} from "@/redux/hook";
 import { addTodo } from "@/redux/features/todoSlice";
+import { useAddTodoMutation } from "@/redux/api/api";
+import { Select } from "@radix-ui/react-select";
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 const AddTodoModal = () => {
   const [task, setTask] = useState("");
   const [description, setDescription] = useState("");
-  const dispatch = useAppDispatch();
+  const [priority, setPriority] = useState("");
 
+  console.log(priority);
+
+  // !For local state management
+  // const dispatch = useAppDispatch();
+
+  // For server
+  // [actualFunctionForPost,{data,isLoading,isError}]
+  const [addTodo, { data,isLoading, isError, isSuccess }] = useAddTodoMutation();
+
+  console.log({ isLoading, isSuccess, isError, data });
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const randomString = Math.random().toString(36).substring(2, 7);
+    // const randomString = Math.random().toString(36).substring(2, 7);
 
     const taskDetails = {
-      id: randomString,
       title: task,
-      description: description,
+      description,
+      isCompleted:false,
+      priority
     };
-    dispatch(addTodo(taskDetails));
+
+    console.log('inside modal',taskDetails);
+    // For local state management
+    // dispatch(addTodo(taskDetails));
+
+    // For Server
+    addTodo(taskDetails)
 
     console.log(taskDetails);
   };
@@ -69,6 +89,19 @@ const AddTodoModal = () => {
                 id="description"
                 className="col-span-3"
               />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label className="text-right">Priority</Label>
+              <Select onValueChange={(value)=>setPriority(value)}>
+                <SelectTrigger className="col-span-3">
+                  <SelectValue placeholder="Priority" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="high">High</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="low">Low</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
           <div className="flex justify-end">
